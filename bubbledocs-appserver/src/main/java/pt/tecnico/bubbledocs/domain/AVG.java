46519485489a83e322f5c36239132b6baae91910
Ricardo Pires <ricardo_pires2@live.com.pt>
2015-03-13 19:15:02 +0000

    .

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/AVG.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/AVG.java
@@ -2,9 +2,17 @@ package pt.tecnico.bubbledocs.domain;
 
 public abstract class AVG extends AVG_Base {
     
-    public AVG() {
+    public AVG(Content args[]) {
         super();
+        super.init(args);
     }
     
+    public int calcula(int args[]){
+    	int somatorio = 1;
+    	for(int i=0;i<args.length;i++)
+    		somatorio += args[i];
+    	
+    	return somatorio;
+    }
 }
 
