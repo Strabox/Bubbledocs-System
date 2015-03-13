2ca2e61f78c567f63e1d6b74ead5937ff1761370
Ricardo Pires <ricardo_pires2@live.com.pt>
2015-03-13 19:13:15 +0000

    Avanço nas IntervalFunctions

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/IntervalFunction.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/IntervalFunction.java
@@ -1,9 +1,36 @@
 package pt.tecnico.bubbledocs.domain;
 
+import java.util.Set;
+import java.util.Vector;
+
 public abstract class IntervalFunction extends IntervalFunction_Base {
     
     public IntervalFunction() {
         super();
     }
+    public void init(Content args[]){
+    	for(int i=0; i<args.length; i++){
+    		addArgument(args[i]);
+    	}
+    }
     
+    
+    @Override
+	public int getResult(){
+    	Set<Content> aux = getArgumentSet();
+    	//int a = aux.size();
+    	int args[];
+    	for(int i=0; i< aux.size(); i++){
+    		args[i] = aux[i];
+    	}
+    	
+		setResultado(calcula(args));
+		return getResultado();
+	}
+    
+    /* 
+     * Calcula - As sublcasses implementam a operação especifica sobre os
+     * argumentos.
+     */
+    public abstract int calcula(int args[]);
 }
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/PRD.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/PRD.java
@@ -2,11 +2,17 @@ package pt.tecnico.bubbledocs.domain;
 
 public abstract class PRD extends PRD_Base {
     
-	public PRD(int arg) {
+	public PRD(Content[] args) {
         super();
-        super.init(arg);
+        super.init(args);
     }
     
-    
+    public int calcula(int args[]){
+    	int piatorio = 1;
+    	for(int i=0;i<args.length;i++)
+    		piatorio *= args[i];
+    	
+    	return piatorio;
+    }
     
 }
