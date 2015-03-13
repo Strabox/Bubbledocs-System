7c4b51d3f41dce76639a645dab856d530c8a8590
Strabox <pardal.pires@gmail.com>
2015-03-13 23:06:57 +0000

    Main...

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/BubbleApplication.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/BubbleApplication.java
@@ -1,7 +1,10 @@
 package pt.tecnico.bubbledocs;
 
-import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
+import pt.ist.fenixframework.TransactionManager;
+import javax.transaction.*;
 import pt.tecnico.bubbledocs.domain.*;
+
 
 public class BubbleApplication {
 	
@@ -12,16 +15,17 @@ public class BubbleApplication {
     }
 	
     /* main - Bubbledocs main function. */
-	@Atomic
 	public static void main(String[] args){
 		System.out.println("Bem-Vindos ao BubbleDocs!!!");
-	
-		Bubbledocs bubble = Bubbledocs.getInstance();
-		/* Se for a 1º vez a correr ele vai preencher a BD com o necessário.*/
-		setupIfNeed(bubble);				
+		TransactionManager tm = FenixFramework.getTransactionManager();
+    	boolean committed = false;				
 		
 		try{
 			/*======== Espaço que contém as chamadas para visualizacao da 1ºEntrega ======*/
+			tm.begin();
+			Bubbledocs bubble = Bubbledocs.getInstance();
+			/* Se for a 1º vez a correr ele vai preencher a BD com o necessário.*/
+			setupIfNeed(bubble);
 			
 			bubble.listAllUsers();
 			
@@ -41,75 +45,11 @@ public class BubbleApplication {
 		
 			System.out.println("??????????????Fim Para Efeitos de Debug??????????????????????");
 			/*============================================================================*/
-			
-			SpreadSheet f = new SpreadSheet("hoi",99,99);
-			Cell cel = new Cell (1,1,new NumberInt(3));
-			Cell cel1 = new Cell (3,3,new NumberInt(8));
-			Cell celm = new Cell (4,4,new MUL(new Reference(cel,1,1),new NumberInt (7)));
-			
-	
-			f.addCel(cel);
-			f.addCel(cel1);
-			f.addCel(celm);
-			
-			
-			for(Cell _cell : f.getCelSet()) {
-				System.out.printf("Linha:%d  Coluna:%d Result:%d\n",_cell.getLine(), _cell.getColumn(),_cell.getContent().getResult());
-				
-			}
-			System.out.println ("Deleting");
-			for(Cell _cell : f.getCelSet()) {
-				_cell.delete();
-				
-			}
-			System.out.println ("Show");
-			for(Cell _cell : f.getCelSet()) {
-				System.out.printf("Linha:%d  Coluna:%d Result:%d\n",_cell.getLine(), _cell.getColumn(),_cell.getContent().getResult());
-				
-			}
 		}
 		catch(Exception e){
 			System.out.println(e);
 		}
 
-		/*		
-		org.jdom2.Document doc = convertToXML();
-
-		printDomainInXML(doc);
-
-		
-
-		org.jdom2.Document doc2 = convertToXML();
-
-		printDomainInXML(doc2);
-
-		recoverFromBackup(doc);
-
-		doc2 = convertToXML();
-
-		printDomainInXML(doc2);
-		*/
 	}
-	/*
-	public static void printDomainInXML(org.jdom2.Document jdomDoc) {
-		XMLOutputter xml = new XMLOutputter();
-		xml.setFormat(Format.getPrettyFormat());
-		System.out.println(xml.outputString(jdomDoc));
-	    }
-	private static void recoverFromBackup(org.jdom2.Document jdomDoc) {
-		Bubbledocs bubble = Bubbledocs.getInstance();
 
-		bubble.importFromXML(jdomDoc.getRootElement());
-	    }
-	   @Atomic
-	    public static org.jdom2.Document convertToXML() {
-		Bubbledocs bubble = Bubbledocs.getInstance();
-		
-		org.jdom2.Document jdomDoc = new org.jdom2.Document();
-
-		jdomDoc.setRootElement(bubble.exportToXML());
-
-		return jdomDoc;
-	    }
-*/
 }
