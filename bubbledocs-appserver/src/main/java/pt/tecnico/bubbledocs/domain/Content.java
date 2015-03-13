e828e5805e43af280ccd2a45e3a095d531da9e3d
carboned4 <slf450x@hotmail.com>
2015-03-13 17:09:00 +0000

    tentar cometer inicio de xml

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Content.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Content.java
@@ -1,5 +1,7 @@
 package pt.tecnico.bubbledocs.domain;
 
+import org.jdom2.Element;
+
 public abstract class Content extends Content_Base {
     
     protected Content() {
@@ -12,5 +14,14 @@ public abstract class Content extends Content_Base {
     
     public abstract int getResult();
     
-    
+    public Element exportToXML(){
+  /*  	Element element = new Element("spreadsheet");
+    	
+    	element.setAttribute("line", Integer.toString(getLine()));
+    	element.setAttribute("column", Integer.toString(getColumn()));
+
+    	element.addContent(getContent().exportToXML());
+
+    	return element;*/ return null;
+    }
 }
