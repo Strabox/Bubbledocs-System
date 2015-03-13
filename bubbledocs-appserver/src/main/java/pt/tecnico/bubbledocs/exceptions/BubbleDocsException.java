d308b07fd7933d3dc567a0ebf56b2fd325dbce68
Strabox <pardal.pires@gmail.com>
2015-03-13 19:02:22 +0000

    ...

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exceptions/BubbleDocsException.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exceptions/BubbleDocsException.java
@@ -1,14 +1,10 @@
 package pt.tecnico.bubbledocs.exceptions;
 
 public class BubbleDocsException extends RuntimeException{
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1724061717573965806L;
 
-	public BubbleDocsException(){
-		super();
-	}
-	
 }
