6c77f0b0979ccdae1b5b45369a032bc1b70b1027
carboned4 <slf450x@hotmail.com>
2015-03-27 22:26:53 +0000

    testes a falhar, commit só para ir buscar coisas ao repo

--- bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/test/AssignReferenceCellTest.java
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/test/AssignReferenceCellTest.java
@@ -10,7 +10,10 @@ import pt.tecnico.bubbledocs.domain.Cell;
 import pt.tecnico.bubbledocs.domain.NumberInt;
 import pt.tecnico.bubbledocs.domain.SpreadSheet;
 import pt.tecnico.bubbledocs.domain.User;
+import pt.tecnico.bubbledocs.exceptions.UnauthorizedOperationException;
+import pt.tecnico.bubbledocs.exceptions.UnknownBubbleDocsUserException;
 import pt.tecnico.bubbledocs.service.AssignReferenceCell;
+import pt.tecnico.bubbledocs.service.DeleteUser;
 
 public class AssignReferenceCellTest extends BubbleDocsServiceTest {
 	
@@ -19,31 +22,35 @@ public class AssignReferenceCellTest extends BubbleDocsServiceTest {
 	private static User userRead;
 	private static User userWrite;
 	private static User userOwner;
-	private static final String USERNAME1 = "usernoperm";
-    private static final String USERNAME2 = "userread";
-    private static final String USERNAME3 = "userwrite";
-    private static final String USERNAME4 = "userowner";
+	private static final String USERNAMENOPERM = "usernoperm";
+    private static final String USERNAMEREAD = "userread";
+    private static final String USERNAMEWRITE = "userwrite";
+    private static final String USERNAMEOWNER = "userowner";
     private SpreadSheet sheet;
 	private Cell withValue;
 	private Cell noValue;
 	private Cell holder;
+	private Cell protect;
 	
 	@Override
     public void populate4Test() {
-        userNoPerm = createUser(USERNAME1, "pass", "no permissions");
-        userRead = createUser(USERNAME2, "pass", "can read");
-        userWrite = createUser(USERNAME3, "pass", "can write");
-        userOwner = createUser(USERNAME4, "pass", "is owner");
+        userNoPerm = createUser(USERNAMENOPERM, "pass", "no permissions");
+        userRead = createUser(USERNAMEREAD, "pass", "can read");
+        userWrite = createUser(USERNAMEWRITE, "pass", "can write");
+        userOwner = createUser(USERNAMEOWNER, "pass", "is owner");
         sheet = createSpreadSheet(userOwner,"sheet1", 3, 3);
         sheet.setOwner(userOwner);
         withValue = new Cell(0,0);	//should have a value
         noValue = new Cell(1,1);	//should have no value
         holder = new Cell(2,2);		//where content is placed
+        protect = new Cell(1,2);	//where content can't be placed
+        protect.setProtect(true);
         NumberInt nint = new NumberInt(42);
         withValue.setContent(nint);
         sheet.addCel(withValue);
         sheet.addCel(noValue);
         sheet.addCel(holder);
+        sheet.addCel(protect);
         bubbled = Bubbledocs.getInstance();
         bubbled.addFolhaCalculo(sheet);
         bubbled.addUtilizador(userNoPerm);
@@ -55,15 +62,52 @@ public class AssignReferenceCellTest extends BubbleDocsServiceTest {
 
     @Test
     public void Control() {
-    	String tokenOwner = addUserToSession(USERNAME4);
+    	String tokenOwner = addUserToSession(USERNAMEOWNER);
     	AssignReferenceCell arcs = new AssignReferenceCell(tokenOwner,sheet.getId(),"2;2","0;0");
-        
     	arcs.execute();
+    	removeUserFromSession(tokenOwner);
     	int result = arcs.getResult();
 		assertEquals("Result of referred cell different from unexpected.", result, 42);
-		
-		removeUserFromSession(tokenOwner);
     }
 	
+    @Test
+    public void userPermissionsOwnerTest() {
+    	String tokenOwner = addUserToSession(USERNAMEOWNER);
+    	AssignReferenceCell arcs = new AssignReferenceCell(tokenOwner,sheet.getId(),"2;2","0;0");
+    	arcs.execute();
+    	removeUserFromSession(tokenOwner);
+    	int result = arcs.getResult();
+		assertEquals("Result of referred cell different from unexpected.", result, 42);
+    }
+    
+    @Test(expected = UnauthorizedOperationException.class)
+    public void userPermissionsReadTest() {
+    	String tokenOwner = addUserToSession(USERNAMEREAD);
+    	AssignReferenceCell arcs = new AssignReferenceCell(tokenOwner,sheet.getId(),"2;2","0;0");
+    	arcs.execute();
+    	removeUserFromSession(tokenOwner);
+    	int result = arcs.getResult();
+		assertEquals("Result of referred cell different from unexpected.", result, 42);
+    }
+    
+    @Test
+    public void userPermissionsWriteTest() {
+    	String tokenOwner = addUserToSession(USERNAMEWRITE);
+    	AssignReferenceCell arcs = new AssignReferenceCell(tokenOwner,sheet.getId(),"2;2","0;0");
+    	arcs.execute();
+    	removeUserFromSession(tokenOwner);
+    	int result = arcs.getResult();
+		assertEquals("Result of referred cell different from unexpected.", result, 42);
+    }
 
+    @Test(expected = UnauthorizedOperationException.class)
+    public void userPermissionsNoneTest() {
+    	String tokenOwner = addUserToSession(USERNAMEOWNER);
+    	AssignReferenceCell arcs = new AssignReferenceCell(tokenOwner,sheet.getId(),"2;2","0;0");
+    	arcs.execute();
+		removeUserFromSession(tokenOwner);
+    	int result = arcs.getResult();
+		assertEquals("Result of referred cell different from unexpected.", result, 42);
+    }
+    
 }
