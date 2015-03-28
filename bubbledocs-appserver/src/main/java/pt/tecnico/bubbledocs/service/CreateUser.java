cd104a89f5f09ce75bfcaed1747f22c7040d69eb
exzamp <exzamp@mbpdemauesteles.home>
2015-03-28 15:47:37 +0000

    CreateUser service

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
@@ -1,23 +1,49 @@
 package pt.tecnico.bubbledocs.service;
 
+import pt.tecnico.bubbledocs.domain.Bubbledocs;
+import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.exceptions.BubbleDocsException;
+import pt.tecnico.bubbledocs.exceptions.UnauthorizedOperationException;
+import pt.tecnico.bubbledocs.exceptions.UserNotInSessionException;
+import pt.tecnico.bubbledocs.exceptions.DuplicateUsernameException;
 
 // add needed import declarations
 
 public class CreateUser extends BubbleDocsService {
 
+	private static final String root = "root"; 
+	
+	private String UserToken;
+	private String newUserUsername;
+	private String newUserPassword;
+	private String newUserIRLName;
+	
     public CreateUser(String userToken, String newUsername,
             String password, String name) {
-	// add code here
+		this.UserToken 	 = userToken;
+		this.newUserUsername = newUsername;
+		this.newUserPassword = password;
+		this.newUserIRLName  = name;
     }
     
     @Override
     protected void accessControl(){
-    	// FIX-ME Implement me!!!!!
+    	Bubbledocs bubble = Bubbledocs.getInstance();
+    	String rootToken  = bubble.getUserInSessionToken(root);
+    	if (UserToken==null || bubble.getUserFromSession(UserToken)==null || rootToken==null)
+    		throw new UserNotInSessionException();
+		else if(!UserToken.equals(rootToken))
+			throw new UnauthorizedOperationException();
     }
     
     @Override
     protected void dispatch() throws BubbleDocsException {
-	// add code here
+    	
+    	Bubbledocs bubble = Bubbledocs.getInstance();
+    	//cannot create two users with the same name
+    	if(bubble.getUserByName(newUserUsername) != null)
+    		throw new DuplicateUsernameException(newUserUsername);
+
+    	bubble.addUtilizador(new User(newUserIRLName,newUserUsername,newUserPassword));
     }
 }
