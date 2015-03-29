8acf2f378b9e3beb20cc6cda12982a8fcce3d9c6
carboned4 <slf450x@hotmail.com>
2015-03-29 13:22:06 +0100

    remoção de importxml da referencia. havia 2 metodos, só 1 é preciso

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Reference.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Reference.java
@@ -66,21 +66,5 @@ public class Reference extends Reference_Base {
     	setColumn(Integer.parseInt(element.getAttribute("column").getValue()));
     }
     
-    public void importFromXML(Element element, Cell container) {
-    	int column = Integer.parseInt(element.getAttribute("line").getValue());
-    	int line = Integer.parseInt(element.getAttribute("column").getValue());
-    	setLine(line);
-    	setColumn(column);
-    	Cell c;
-    	// Checks if there already is a cell in the spreadsheet (accessed using the containing cell.
-    	c = container.getFc().getSingleCell(line, column);
-    	if(c != null){
-    		setRefCell(c);
-    		return; // Success: sets the cell as referred to, then exits.
-    	}
-    	//Failure: creates an "empty" cell that may later have a content
-    	c = new Cell(line, column);
-    	container.getFc().addCell(c);
-    	setRefCell(c);
-    }
+    
 }
