c05486eb4978eaad8718f32ae0d61f61b67b1e63
Strabox <pardal.pires@gmail.com>
2015-03-31 20:35:15 +0100

    Setuping Up....

---
+++ sd-id/src/main/java/sdis/SDImpl.java
@@ -0,0 +1,42 @@
+package sdis;
+
+import javax.jws.*;
+
+import pt.ulisboa.tecnico.sdis.id.ws.*; // classes generated from WSDL
+
+@SuppressWarnings("restriction")
+@WebService(
+    endpointInterface="pt.ulisboa.tecnico.sdis.id.ws.SDId", 
+    wsdlLocation="SD-ID.1_1.wsdl",
+    name="SdId",
+    portName="SDIdImplPort",
+    targetNamespace="urn:pt:ulisboa:tecnico:sdis:id:ws",
+    serviceName="SDId"
+)
+public class SDImpl implements SDId {
+
+
+	public void createUser(String userId, String emailAddress)
+			throws EmailAlreadyExists_Exception, InvalidEmail_Exception,
+			InvalidUser_Exception, UserAlreadyExists_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void renewPassword(String userId) throws UserDoesNotExist_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeUser(String userId) throws UserDoesNotExist_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public byte[] requestAuthentication(String userId, byte[] reserved)
+			throws AuthReqFailed_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
