84b1bbd19a779adbaeef6c0d88d8e2bfbcb3ff59
carboned4 <slf450x@hotmail.com>
2015-04-16 23:29:26 +0100

    [SD] UserTest

--- sd-id/src/test/java/sdis/test/UserTest.java
+++ sd-id/src/test/java/sdis/test/UserTest.java
@@ -1,11 +1,55 @@
 package sdis.test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import sdis.domain.User;
 
 public class UserTest {
 
+	User user1;
+	private final String username1 = "user1";
+	private final String email1 = "user1";
+	
+	User user2;
+	private final String username2 = "user2";
+	private final String email2 = "user2";
+	
+	@Before
+	public void populate4Test(){
+		user1 = new User(username1, email1);
+		user2 = new User(username2, email2);
+	}
+	
 	@Test
-	public void success(){
-		
+	public void testVerifyPasswordCorrect(){
+		String passBefore = user1.getPassword();
+		assertTrue(user1.verifyPassword(passBefore));
+	}
+	
+	@Test
+	public void testVerifyPasswordIncorrect(){
+		String passBefore = user1.getPassword();
+		assertFalse(user1.verifyPassword(passBefore+"aaa"));
+	}
+	
+	@Test
+	public void testVerifyPasswordNull(){
+		assertFalse(user1.verifyPassword(null));
+	}
+	
+	@Test
+	public void testVerifyPasswordEmpty(){
+		assertFalse(user1.verifyPassword(""));
+	}
+	
+	@Test
+	public void testSetNewPassword(){
+		String passBefore = user2.getPassword();
+		String passAfter = user2.setNewPassword();
+		assertFalse(passBefore == passAfter);
 	}
 }
