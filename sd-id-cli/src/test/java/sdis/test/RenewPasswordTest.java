b268ec742d98867eace2212f213e822f34443f55
carboned4 <slf450x@hotmail.com>
2015-04-16 02:06:00 +0100

    [SD] Renew tests

--- sd-id-cli/src/test/java/sdis/test/RenewPasswordTest.java
+++ sd-id-cli/src/test/java/sdis/test/RenewPasswordTest.java
@@ -1,5 +1,38 @@
 package sdis.test;
 
-public class RenewPasswordTest extends SdIdTest {
+import static org.junit.Assert.fail;
 
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.sdis.id.ws.UserDoesNotExist_Exception;
+
+public class RenewPasswordTest extends SdIdTest {
+	private final String usernameExists1 = "carla";
+	private final String usernameDoesntExist = "renewSD";
+	
+	@Test
+	public void renewPasswordTestSuccess() {
+		try{
+			idServer.renewPassword(usernameExists1);
+			// ATTENTION: don't we need to make sure the new password is indeed new?
+		}
+		catch(Exception e){
+			fail();
+		}
+	}
+	
+	@Test(expected = UserDoesNotExist_Exception.class)
+	public void renewPasswordTestNoUser() throws UserDoesNotExist_Exception{
+		idServer.renewPassword(usernameDoesntExist);
+	}
+	
+	@Test(expected = UserDoesNotExist_Exception.class)
+	public void renewPasswordTestNullUsername() throws UserDoesNotExist_Exception{
+		idServer.renewPassword(null);
+	}
+	
+	@Test(expected = UserDoesNotExist_Exception.class)
+	public void renewPasswordTestEmptyUsername() throws UserDoesNotExist_Exception{
+		idServer.renewPassword("");
+	}
 }
