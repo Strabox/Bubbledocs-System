65fdc5f901156f68141a1fbdf529369d7031a8a1
exzamp <exzamp@mbpdemauesteles.home>
2015-04-17 01:57:41 +0100

    RemoveUserTest

---
+++ sd-id-cli/src/test/java/sdis/test/RemoveUserTest.java
@@ -0,0 +1,34 @@
+package sdis.test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import pt.ulisboa.tecnico.sdis.id.ws.EmailAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidEmail_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidUser_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.UserAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.UserDoesNotExist_Exception;
+
+public class RemoveUserTest extends SdIdTest {
+	
+	private final String ExistingUsername = "duarte";
+	private final String MissingUsername = "Adama";
+	
+	/* Remove a user that does exist in the SDID. */
+	@Test
+	public void RemoveExistingUser() {
+		try{
+			idServer.removeUser(ExistingUsername);
+		}
+		catch(Exception e){
+			fail(e.toString());
+		}
+	}
+
+	// Remove a user that does not exist in the SDID 
+	@Test(expected = UserDoesNotExist_Exception.class)
+	public void RemoveMissingUser() throws UserDoesNotExist_Exception{
+			idServer.removeUser(MissingUsername);
+	}
+}
