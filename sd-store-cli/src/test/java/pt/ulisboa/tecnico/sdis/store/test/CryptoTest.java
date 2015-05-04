dc6f63ad485fe98f5b53e672377bde358b9513ad
arturfonseca <artur.fonseca@live.com>
2015-05-04 19:29:44 +0100

    crypto tests

---
+++ sd-store-cli/src/test/java/pt/ulisboa/tecnico/sdis/store/test/CryptoTest.java
@@ -0,0 +1,26 @@
+package pt.ulisboa.tecnico.sdis.store.test;
+
+import static org.junit.Assert.*;
+import pt.ulisboa.tecnico.sdis.store.cli.Crypto;
+import org.junit.Test;
+
+
+
+
+
+public class CryptoTest extends SDStoreTest {
+	
+	@Test
+	public void cypherSuccess() throws Exception {
+		String expectedString ="ABCDabcd123456";
+		Crypto crypto = new Crypto();
+		byte[] content= expectedString.getBytes();
+		byte[] encryptedContent=crypto.encrypt(content);
+		String resultString=new String(crypto.decrypt(encryptedContent));
+		assertEquals("crypto1",expectedString,resultString);
+	}
+	
+
+	
+	
+}
