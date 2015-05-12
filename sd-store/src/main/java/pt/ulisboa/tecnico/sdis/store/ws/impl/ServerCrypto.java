502e1f462e01974f2d97cbe8eef0f41d9237eaf7
Ricardo Pires <ricardo_pires2@live.com.pt>
2015-05-12 19:45:55 +0100

    MAC implemented.

--- sd-store/src/main/java/pt/ulisboa/tecnico/sdis/store/ws/impl/ServerCrypto.java
+++ sd-store/src/main/java/pt/ulisboa/tecnico/sdis/store/ws/impl/ServerCrypto.java
@@ -1,62 +1,11 @@
-package pt.ulisboa.tecnico.sdis.store.cli;
+package pt.ulisboa.tecnico.sdis.store.ws.impl;
 
-import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 
-public class Crypto{
-	
-	private final Key secretKey;
-	private final Cipher cipher;
-	
-	
-	public Crypto() throws NoSuchAlgorithmException, NoSuchPaddingException{
-		KeyGenerator keyGen = KeyGenerator.getInstance("DES");
-        keyGen.init(56);
-        secretKey = keyGen.generateKey();
-        cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
-	}
-	
-	
-	public byte[] encrypt(byte[] file){
-		byte[] cipherFile = null;
-		try {
-			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-			cipherFile = cipher.doFinal(file);
-		} catch (InvalidKeyException e) {
-			System.out.println("Key is invalid");
-		} catch (IllegalBlockSizeException e) {
-			System.out.println("Illegal Block Size");
-		} catch (BadPaddingException e) {
-			System.out.println("Bad Padding Exception");
-		}
-        
-		return cipherFile;
-	}
-	
-	public byte[] decrypt(byte[] cipherFile){
-		byte[] file = null;
-		try {
-			cipher.init(Cipher.DECRYPT_MODE, secretKey);
-	        file = cipher.doFinal(cipherFile);
-		} catch (InvalidKeyException e) {
-			System.out.println("Key is invalid");
-		} catch (IllegalBlockSizeException e) {
-			System.out.println("Illegal Block Size");
-		} catch (BadPaddingException e) {
-			System.out.println("Bad Padding Exception");
-		}
-        
-		return file;
-	}
+public class ServerCrypto{
 	
 	public final byte[] makeMAC(byte[] bytes, SecretKey key) throws Exception {
 
